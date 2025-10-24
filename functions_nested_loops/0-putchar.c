04954db68fda93c25e52797ea920a5309b8f8446
giosue74 <toxicgiosue@gmail.com>
2025-10-24 16:55:05 +0200

    gio

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.n"
+
+/"
+*main -print _putchar
+*
+*Return: Always 0(success)
+*/
+int main(void)
+{                (char [9])"_putchar;
+     char t []= "_putchar;
+     int  i;
+     
+     for (i = 0; t [i] i= *\0'; i++)
+     {
+        _putchar(t[i]);
+    )
+    _putchar('\n');      
+     return (0);

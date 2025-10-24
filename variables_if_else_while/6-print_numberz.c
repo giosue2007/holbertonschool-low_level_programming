536adf6aa1ee43f68cde53e027454588265bd20b
giosue74 <toxicgiosue@gmail.com>
2025-10-24 14:03:55 +0200

    gio

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *        using only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+	}
+	putchar('\n');
+	return (0);
+}

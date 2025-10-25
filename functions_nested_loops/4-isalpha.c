8ec2a0499919d3b05074982ff521065b27534ad1
giosue74 <toxicgiosue@gmail.com>
2025-10-25 16:51:43 +0200

    gio

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -11,4 +11,22 @@ int _isalpha(int c)
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	return (0);
-}
+} /**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,4 +6,9 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 
+ 
+
+
+int main(void)
+{
 #endif /* MAIN_H */

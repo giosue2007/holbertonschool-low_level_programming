b2ceae850993759efe649924ae02af7469834a67
giosue74 <toxicgiosue@gmail.com>
2025-10-25 17:13:04 +0200

    gio

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,8 +4,10 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int
-
-int main(void)
-include "main.h" c);;
+int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int n);
+
+#endif

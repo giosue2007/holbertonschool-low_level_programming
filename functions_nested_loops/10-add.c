b667cbc63d937d1a7f1b03d33ade78218de26320
giosue74 <toxicgiosue@gmail.com>
2025-10-26 12:56:01 +0100

    gio

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,4 +1,13 @@
+#include "main.h"
+
+/**
+* add - Checks two for integers and return the result
+* @x: 2nd integer to add.
+*@y:2nd integer to add
+*
+* Return: Always 0
+*/
 int add(int x, int y)
 {
-return (x + y);
+	return (x + y);
 }

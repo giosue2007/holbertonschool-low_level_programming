7fb627d9f6db2f7d6d20c3a82888ab93caae3ae5
giosue74 <toxicgiosue@gmail.com>
2025-10-27 14:52:52 +0100

    gio

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers.c - check the code.
+ *
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+
+}
+
+int main(void)
+{
+    more_numbers();
+    return (0);
+}

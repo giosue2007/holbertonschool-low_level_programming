43002d6d6f2e013a5630dafa6e5c3c778db5e16e
giosue74 <toxicgiosue@gmail.com>
2025-10-27 13:51:04 +0100

    gio

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,23 +3,16 @@
 /**
  * print_numbers - check the code.
  *
- * @c: character to be checked
+ *
  * Return: Always 0.
  */
 void print_numbers(void)
 {
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	else
-	{
-		_putchar
-	}
-}
+	char c;
 
-int main(void)
-{
-    print_numbers();
-    return (0);
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }

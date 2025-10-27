082a44b2496a9479623ff4528b07d41cd9ecff2f
giosue74 <toxicgiosue@gmail.com>
2025-10-27 09:10:30 +0100

    gio

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* add - Checks two for integers and return the result
-* @x: 2nd integer to add.
-* @y:2nd integer to add.
-*
-* Return: Always 0
-*/
-int add(int x, int y)
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the number to enter
+ *
+ * Return: Always nothing
+ */
+void print_to_98(int n)
 {
-	return (x + y);
+	if (n != 98)
+	{
+		printf("%d, ", n);
+	}
+	else
+	{
+		printf("98\n");
+	}
+
+	while (n != 98)
+	{
+		if (n < 98)
+		{
+			n++;
+		}
+		else
+		{
+			n--;
+		}
+
+		if (n != 98)
+		{
+			printf("%d, ", n);
+		}
+		else
+		{
+			printf("98\n");
+		}
+	}
 }

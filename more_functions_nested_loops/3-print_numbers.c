0f664351f3e9d339fead68295079c7d37ad3b87f
giosue74 <toxicgiosue@gmail.com>
2025-10-27 13:10:12 +0100

    gio

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _isdigit - check the code.
+ * _print_number - check the code.
  *
  * @c: character to be checked
  * Return: Always 0.
  */
-int _isdigit(int c)
+int _print_number(int c)
 {
 	if (c >= '0' && c <= '9')
 	{

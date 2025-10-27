f55fc9fca79e67c35a5844c9e7a7af06eb2f81a4
giosue74 <toxicgiosue@gmail.com>
2025-10-27 15:45:41 +0100

    gio

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * more_numbers.c - check the code.
- *
- *
- * Return: Always 0.
- */
+* more_numbers - numbers, from 0 to 14
+*
+*
+* Return: Always 0.
+*/
 void more_numbers(void)
 {
-
+	int c;
+	int i = 0;
+	while (i < 10)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+			}
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+		i++;
+	}
 }
 
 int main(void)

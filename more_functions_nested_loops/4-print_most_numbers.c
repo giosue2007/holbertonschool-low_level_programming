93816e55bd2ca15a3d9fc27ec4cff3b8e5956dfe
giosue74 <toxicgiosue@gmail.com>
2025-10-27 14:40:39 +0100

    gio

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - check the code.
+ *
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != (2 + '0') && c != (4 + '0'))
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
+
+int main()
+{
+	print_most_numbers();
+	return 0;
+}

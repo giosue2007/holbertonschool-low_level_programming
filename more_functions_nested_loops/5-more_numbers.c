bfbf171e431ec6cacc2264671cb79e2b1ed2178f
giosue74 <toxicgiosue@gmail.com>
2025-10-28 09:00:58 +0100

    gio

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,16 +11,17 @@ void more_numbers(void)
 	int c;
 	int i = 0;
 
-	while (i < 14)
+	while (i < 10)
 	{
 		for (c = 0; c <= 14; c++)
 		{
 			if (c > 9)
 			{
-				_putchar((c / 14) + '0');
+				_putchar((c / 10) + '0');
 			}
-			_putchar((c % 14) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 		i++;
 	}
+}

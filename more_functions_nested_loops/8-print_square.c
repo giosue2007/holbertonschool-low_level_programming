f3aa919ef15d366686e3d4728e18f8ac5dd48350
giosue74 <toxicgiosue@gmail.com>
2025-10-28 11:43:53 +0100

    gio

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,29 +8,28 @@
 */
 void print_square(int size)
 {
-	int i;
+int i, j;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-
-	for (i = 0; i < size; i++)
-	{
-    	int i2;
-
-		for (i2= 0; i2 < size; i2++)
-		{
-    		_putchar('#');
-		}
-		_putchar('\n');
-	}
+if (size <= 0)
+{
+    _putchar('\n');
 }
-
+else
+{
+    for (i = 0; i < size; i++)
+    {
+    	for(j=0; j < size; j++)
+        {
+          _putchar('#');
+        }
+    _putchar('\n');
+    }
+    }
+}
 int main(void)
 {
-    print_square(2);
-    print_square(10);
-    print_square(0);
-    return (0);
+print_square(2);
+print_square(10);
+print_square(0);
+return (0);
 }

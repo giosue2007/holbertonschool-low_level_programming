f43424d032154f79df7f881cc1ddb3240c19297d
giosue74 <toxicgiosue@gmail.com>
2025-12-03 16:52:57 +0100

    8

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,10 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * print_most_numbers - check the code.
- *
- *
- * Return: Always 0.
- */
-void print_most_numbers(void)
-{
-	char c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		if (c != (2 + '0') && c != (4 + '0'))
-		{
-			_putchar(c);
-		}
-	}
-	_putchar('\n');
+void tester(char *nom_du_test, int obtenu, int attendu) {
+    if (obtenu == attendu) {
+        printf("%s : OK\n", nom_du_test);
+    } else {
+        printf("%s : FAUX (obtenu %d, attendu %d)\n", 
+            nom_du_test, obtenu, attendu);
+    }
 }

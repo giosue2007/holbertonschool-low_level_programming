20a019aef484f8aa0324e409bb9dd2c7b5de5354
giosue74 <toxicgiosue@gmail.com>
2025-12-04 14:59:38 +0100

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
-void tester(char *nom_du_test, int obtenu, int attendu) {
-    if (obtenu == attendu) {
-        printf("%s : OK\n", nom_du_test);
-    } else {
-        printf("%s : FAUX (obtenu %d, attendu %d)\n", 
-            nom_du_test, obtenu, attendu);
-    }
+/*
+   pointeur : variables contenant l'adresse d'une autre variables
+
+[VARIABLES]
+    maVariable  : valeur de la variables
+    &maVariables : adresse de la
+
+
+int inverser_nombre(nombreA,nombrA)
+{
+    int temporaire =0,//B
+
+	temporaire = nombreB
+	nombreB = nombreA;
+	nombreA = temporaire;
+
+	return nombreA;
 }
+int main(void)	
+{
+
+    int nombreA = 15;
+	int nombreB = 28;
+
+	printf(Adresse de NombreB = %p\n",&nombreB);
+
+//printf(AVANT : A = %d et B =)

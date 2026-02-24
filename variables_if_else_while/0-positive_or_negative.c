d13460b18de34bfa1f4e8dcefc2ef2f8dd37b72e
giosue2007 <toxicgiosue@gmail.com>
2026-02-24 10:19:59 +0100

    add perm and c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,29 +3,32 @@
 #include <stdio.h>
 
 /**
- * main - point d'entrée du programme
- * 
- * Description Affiche si le nombre n est positif,  négatif ou zéro
- * 
- * Return Toujours 0 (Succès)
+ * main - Point d'entrée du programme
+ *
+ * Description: Affiche si le nombre n est positif, négatif ou zéro
+ *
+ * Return: Toujours 0 (Succès)
  */
 int main(void)
 {
-	      int n; 
-		  
-		  srand(time(0));
-		 n = rand()  - RAND_MAX / 2;
+	int n;
 
-		 /* Ton code commence ici */
-		 if (n > 0)
-		 {
-			     printf("%d is positive\n", n);
-		 }
-		 else if (n == 0)
-		 {
-			      printf("%d is zero\n", n);
-		 }
-		 
-		  return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* Ton code commence ici */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
 }
-		

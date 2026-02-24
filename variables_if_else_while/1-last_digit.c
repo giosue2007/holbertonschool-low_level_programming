82d5f4b47c03eb517094e0c090cd9a4e8fdc7c78
giosue2007 <toxicgiosue@gmail.com>
2026-02-24 11:07:50 +0100

    add perm and c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	/* On calcule le dernier chiffre ici */
+	last = n % 10;
+
+	printf("Last digit of %d is %d ", n, last);
+
+	if (last > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (last == 0)
+	{
+		printf("and is 0\n");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+
+	return (0);
+}

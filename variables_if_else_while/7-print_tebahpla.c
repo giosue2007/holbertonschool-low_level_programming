616e094951f342142a7b630fc003f5493e61503c
giosue2007 <toxicgiosue@gmail.com>
2026-02-25 11:11:43 +0100

    add perm and c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/** 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+int main(void)
+{
+         char lettre;
+
+		 for (lettre = 'z'; lettre >= 'a'; lettre--)
+		 {
+			     putchar(lettre);
+		 }
+		 putchar('/');
+
+		 return (0);
+}

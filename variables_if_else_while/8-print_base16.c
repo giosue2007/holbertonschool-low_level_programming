9c07975580b48b2dfb0738bf76cf767eadf2a7b2
giosue2007 <toxicgiosue@gmail.com>
2026-02-25 12:22:51 +0100

    add perm and c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - Point d'entrée
- *
- * Description: Affiche les chiffres de la base 16 (0-9 et a-f)
- * en utilisant seulement 3 putchar.
- *
- * Return: Toujours 0
+* main - Point d'entrée
+*
+* Description: Affiche les chiffres de la base 16 (0-9 et a-f)
+* en utilisant seulement 3 putchar.
+*
+* Return: Toujours 0
  */
 int main(void)
 {
-	    char c;
-		
-		/* 1. Boucle pour les chiffre de '0' a '9' */
+		 char c;
+				/* 1. Boucle pour les chiffre de '0' a '9' */
 		for (c = '0'; c <= '9'; c++)
-        {
+	{
 			putchar(c); /* Premier putchar */
-		}
+	}
 
 		/* 2. Boucle pour les lettre de "a" à 'f' */
-		for (c = 'a'; c <=n 'f'; c++)
+		for (c = 'a'; c <= 'f'; c++)
+
 		{
 			putchar(c); /* Deuxime putchar*/
 		}
 
 		putchar('\n'); /* troisième putchar */
+
 
 		return (0);
 }

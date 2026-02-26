27b3aab605300abb6b9d49754884eb289c82f4d5
giosue2007 <toxicgiosue@gmail>
2026-02-26 16:10:10 +0100

    add perm and c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Additionne deux entiers
+ * @a: Le premier entier
+ * @b: Le deuxième entier
+ *
+ * Return: La somme de a et b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

c4e40e4d2c134f24e3a03918e16908a2de1f5e5f
giosue2007 <toxicgiosue@gmail>
2026-02-26 16:09:10 +0100

    add perm and c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,6 +2,12 @@
 #define MAIN_H
 
 int _putchar(char c);
-void jack_bauer(void); /* <--- AJOUTE CETTE LIGNE ICI */
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void); /* <--- AJOUTE CETTE LIGNE ICI */
 
 #endif

54899f0212f62ec355542a0c3d79f6f578abc1db
giosue2007 <toxicgiosue@gmail.com>
2026-02-26 10:51:24 +0100

    add perm and c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Point d'entrée
+ *
+ * Description: Affiche le mot _putchar suivi d'un retour à la ligne.
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}

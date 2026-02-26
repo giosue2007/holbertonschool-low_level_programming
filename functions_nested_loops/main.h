45459093658aa422eda1c676a07c1fb4a7b6598a
giosue2007 <toxicgiosue@gmail>
2026-02-26 15:55:48 +0100

    add perm and c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Prototypes des fonctions précédentes */
 int _putchar(char c);
+int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+
+/* Prototype pour l'exercice 7 */
+int print_last_digit(int); /* <--- AJOUTE CETTE LIGNE ICI */
 
 #endif

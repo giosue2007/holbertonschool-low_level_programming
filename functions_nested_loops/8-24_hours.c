05eb71ee5d7719084d4d0f6c11c21fa33cbb6a38
giosue2007 <toxicgiosue@gmail>
2026-02-26 16:05:28 +0100

    add perm and c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - Affiche chaque minute de la journée
- *
- * Description: De 00:00 à 23:59
+ * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void)
 {
@@ -13,18 +11,11 @@ void jack_bauer(void)
 	{
 		for (m = 0; m <= 59; m++)
 		{
-			/* Affichage des dizaines de l'heure */
 			_putchar((h / 10) + '0');
-			/* Affichage des unités de l'heure */
 			_putchar((h % 10) + '0');
-			
 			_putchar(':');
-			
-			/* Affichage des dizaines de la minute */
 			_putchar((m / 10) + '0');
-			/* Affichage des unités de la minute */
 			_putchar((m % 10) + '0');
-			
 			_putchar('\n');
 		}
 	}

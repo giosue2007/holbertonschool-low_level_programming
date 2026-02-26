f23f27ff7277d49e6138af773bc855c29d7b7189
giosue2007 <toxicgiosue@gmail>
2026-02-26 16:22:48 +0100

    add perm and c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -11,4 +11,6 @@ void jack_bauer(void);
 void times_table(void); /* <--- AJOUTE CETTE LIGNE ICI */
 int add(int, int);
 void print_to_98(int n);
+void print_to_98(int n);
+void print_to_98(int n);
 #endif

d758b5b378ded642bfc9380d67d05d84505617b4
giosue2007 <toxicgiosue@gmail>
2026-02-27 16:19:06 +0100

    add perm and c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,14 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Le prototype de la fonction putchar fournie par l'école */
 int _putchar(char c);
-
-/* Les prototypes de tes fonctions (tes "cartes d'identité") */
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-void print_diagonal(int n); /* <--- C'EST CETTE LIGNE QU'IL TE MANQUE ! */
+void print_diagonal(int n);
+void print_square(int size); /* <--- Vérifie si elle est là avec le ; */
 
 #endif

f2b3ba2df1355068611ece80de79b6a42278f49d
giosue2007 <toxicgiosue@gmail>
2026-02-27 13:47:11 +0100

    add perm and c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _insupper - Vérifie si un caractere est une majusqule.
+ * @c: Le caractère à verifier (représenté par son code ASCII).
+ * 
+ * Return: 1 si c'est une majuscule, 0 sinon,
+ */
+int_insupper(int c)
+{
+	/*En ASCII, les majuscule vont de 'a' (65 ) à 'z' (90)*/
+	if (c >= 'A' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

8c68d78e12427aee79e315e174be656a7e3546ee
giosue2007 <toxicgiosue@gmail>
2026-02-27 15:30:26 +0100

    add perm and c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplie deux entiers.
+ * @a: Premier entier.
+ * @b: Deuxième entier.
+ *
+ * Return: Le résultat de la multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

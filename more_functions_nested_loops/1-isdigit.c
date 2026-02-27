4e496371f1557e1c7bc4a0cb9fc2d8393fc37cbc
giosue2007 <toxicgiosue@gmail>
2026-02-27 14:44:43 +0100

    add perm and c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
  * _isdigit - vèrifie si un caractère est un chiffre (0 à 9).
  * @c: Le caractère (sous forme d'entier) à vérifier
- * 
+ *
  * Return: 1 si c est in chiffre, 0 sinon.
  */
 int _isdigit(int c)
@@ -10,10 +10,10 @@ int _isdigit(int c)
 		/*Vérifier si c est compris entre le code ASCII de '0' et '9' */
 		if (c >= '0' && c <= '9')
 		{
-				  return (1);
+			return (1);
 		}
 		else
 		{
-				 return (0);
+			return (0);
 		}
 }

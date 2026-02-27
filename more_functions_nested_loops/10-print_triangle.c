acb53818e6a427c35bbc037897fdd315b890ca05
giosue2007 <toxicgiosue@gmail>
2026-02-27 16:27:57 +0100

    add perm and c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,21 +6,21 @@
  */
 void print_triangle(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (i = 1; i <= size; i++)
-        {
-            for (j = 0; j < (size - i); j++)
-                _putchar(' ');
-            for (j = 0; j < i; j++)
-                _putchar('#');
-            _putchar('\n');
-        }
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < (size - i); j++)
+				_putchar(' ');
+			for (j = 0; j < i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
 }

09c4ef108717a5f6495234ad39e578665581a064
giosue2007 <toxicgiosue@gmail>
2026-02-27 16:13:41 +0100

    add perm and c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Prototype de _putchar */
 int _putchar(char c);
+
+/* Prototypes des exercices précédents */
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(void)
-#include "main.h"
 
-void print_line(int n)  /* <--- ERREUR : Si tu mets un point-virgule ici alors que tu écris la fonction */
-{
-    // ...
-}
+/* Prototype de l'exercice actuel */
+void print_line(int n);
+
 #endif

ce14ed7dbc6ea559ea47e6ed17953ba5471d5dab
giosue2007 <toxicgiosue@gmail>
2026-02-27 14:03:28 +0100

    add perm and c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
 int _putchar(char c);
-int_insupper(int c)
-int _isupper(int c)
+int _isupper(int c); /* <-- Vérifie bien l'espace, l'orthographe et le ; */
+
 #endif

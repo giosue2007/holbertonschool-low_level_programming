5f2c836c50c40156aae71357413617cf58f18595
giosue2007 <toxicgiosue@gmail>
2026-02-27 16:12:38 +0100

    add perm and c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * print_line - Dessine une ligne droite dans le terminal.
- * @n: Le nombre de fois que le caractère '_' doit être imprimé.
+ * print_line - draws a straight line in the terminal.
+ * @n: number of times the character _ should be printed.
  */
 void print_line(int n)
 {
 	int i;
 
-	/* 1. Si n est supérieur à 0, on lance la boucle pour dessiner */
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
@@ -16,7 +15,5 @@ void print_line(int n)
 			_putchar('_');
 		}
 	}
-
-	/* 2. Dans TOUS les cas (n > 0 ou n <= 0), on finit par un saut de ligne */
 	_putchar('\n');
 }

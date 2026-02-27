4533981f96024b01240961e734ea1e06300082ab
giosue2007 <toxicgiosue@gmail>
2026-02-27 09:12:59 +0100

    add perm and c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,34 +2,28 @@
 #include "main.h"
 
 /**
- * print_to_98 - imprime tous les nombres naturels de n à 98.
- * @n: le nombre de départ.
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the number to start from
  */
 void print_to_98(int n)
 {
-    /* Cas 1 : n est inférieur ou égal à 98 */
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            printf("%d", n);
-            if (n != 98)
-            {
-                printf(", ");
-            }
-        }
-    }
-    /* Cas 2 : n est supérieur à 98 */
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            printf("%d", n);
-            if (n != 98)
-            {
-                printf(", ");
-            }
-        }
-    }
-    printf("\n");
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
 }

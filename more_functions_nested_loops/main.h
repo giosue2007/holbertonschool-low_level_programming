06c61a88671dc30761ba411fef9af197d1d46a53
giosue2007 <toxicgiosue@gmail>
2026-02-27 15:57:14 +0100

    add perm and c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,10 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b); /* <-- IL FAUT AJOUTER CETTE LIGNE ICI */
 void print_numbers(void);
-int _putchar(char c);
+void print_most_numbers(void);
 
 #endif

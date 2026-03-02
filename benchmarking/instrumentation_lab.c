df3ea9bd0f16c512976fab3da14be72df22d4ded
giosue2007 <toxicgiosue@gmail>
2026-03-02 15:33:43 +0100

    add perm and c

--- benchmarking/instrumentation_lab.c
+++ benchmarking/instrumentation_lab.c
@@ -8,67 +8,86 @@ static int dataset[DATASET_SIZE];
 
 static unsigned int next_value(unsigned int *state)
 {
-    *state = (*state * 1103515245u) + 12345u;
-    return *state;
+	*state = (*state * 1103515245u) + 12345u;
+	return (*state);
 }
 
 static void build_dataset(void)
 {
-    unsigned int state;
-    int i;
+	unsigned int state;
+	int i;
 
-    state = SEED_VALUE;
+	state = SEED_VALUE;
 
-    for (i = 0; i < DATASET_SIZE; i++)
-        dataset[i] = (int)(next_value(&state) % 100000);
+	for (i = 0; i < DATASET_SIZE; i++)
+		dataset[i] = (int)(next_value(&state) % 100000);
 }
 
 static void process_dataset(void)
 {
-    int i;
-    int v;
+	int i;
+	int v;
 
-    for (i = 0; i < DATASET_SIZE; i++)
-    {
-        v = dataset[i];
-        v = (v * 3) + (v / 7) - (v % 11);
-        if (v < 0)
-            v = -v;
-        dataset[i] = v;
-    }
+	for (i = 0; i < DATASET_SIZE; i++)
+	{
+		v = dataset[i];
+		v = (v * 3) + (v / 7) - (v % 11);
+		if (v < 0)
+			v = -v;
+		dataset[i] = v;
+	}
 }
 
 static unsigned long reduce_checksum(void)
 {
-    unsigned long sum;
-    int i;
+	unsigned long sum;
+	int i;
 
-    sum = 0;
-    for (i = 0; i < DATASET_SIZE; i++)
-        sum = (sum * 131ul) + (unsigned long)dataset[i];
+	sum = 0;
+	for (i = 0; i < DATASET_SIZE; i++)
+		sum = (sum * 131ul) + (unsigned long)dataset[i];
 
-    return sum;
+	return (sum);
 }
 
 int main(void)
 {
-    unsigned long checksum;
+	unsigned long checksum;
 
-    /* Students must add clock-based timing and print required lines. */
+	clock_t total_start, total_end;
+	clock_t start, end;
 
-    build_dataset();
-    process_dataset();
-    checksum = reduce_checksum();
+	double total_sec;
+	double build_sec;
+	double process_sec;
+	double reduce_sec;
 
-    if (checksum == 0ul)
-        printf("impossible\n");
+	total_start = clock();
 
-    /* Required output (exact format, no extra lines):
-     * TOTAL seconds: <float>
-     * BUILD_DATA seconds: <float>
-     * PROCESS seconds: <float>
-     * REDUCE seconds: <float>
-     */
+	start = clock();
+	build_dataset();
+	end = clock();
+	build_sec = (double)(end - start) / (double)CLOCKS_PER_SEC;
 
-    return 0;
+	start = clock();
+	process_dataset();
+	end = clock();
+	process_sec = (double)(end - start) / (double)CLOCKS_PER_SEC;
+
+	start = clock();
+	checksum = reduce_checksum();
+	end = clock();
+	reduce_sec = (double)(end - start) / (double)CLOCKS_PER_SEC;
+
+	total_end = clock();
+	total_sec = (double)(total_end - total_start) / (double)CLOCKS_PER_SEC;
+
+	(void)checksum;
+
+	printf("TOTAL seconds: %.6f\n", total_sec);
+	printf("BUILD_DATA seconds: %.6f\n", build_sec);
+	printf("PROCESS seconds: %.6f\n", process_sec);
+	printf("REDUCE seconds: %.6f\n", reduce_sec);
+
+	return (0);
 }

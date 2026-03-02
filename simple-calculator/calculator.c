7ce5f2f943a03e04ea56b3fa093b45ae634e717c
giosue2007 <toxicgiosue@gmail>
2026-03-02 19:48:49 +0100

    add perm and c

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,41 +1,38 @@
 #include <stdio.h>
 
+/**
+ * main - Point d'entrée du programme
+ * Return: Toujours 0
+ */
 int main(void)
 {
     int choice;
 
-    // 1. Ligne de bienvenue
+    /* 1. Ligne de bienvenue */
     printf("Simple Calculator\n");
 
-    // 2. Boucle du menu
     do {
-        // Affichage du menu
+        /* Affichage du menu */
         printf("1) Add\n");
         printf("2) Subtract\n");
         printf("3) Multiply\n");
         printf("4) Divide\n");
         printf("0) Quit\n");
 
-        // Récupération du choix
         printf("Choice: ");
-        scanf("%d", &choice);
+        /* Lecture du choix */
+        if (scanf("%d", &choice) != 1)
+        {
+            /* Petite sécurité si l'utilisateur tape une lettre */
+            break;
+        }
 
-        // 3. Gestion du choix "0"
+        /* 3. Gestion du choix "0" */
         if (choice == 0)
         {
             printf("Bye!\n");
         }
-        else if (choice >= 1 && choice <= 4)
-        {
-            // Pour l'instant, on touche rien c'est juste le squelette
-            printf("Option %d selected (not implemented yet)\n", choice);
-        }
-        else
-        {
-            printf("Invalid choice, try again.\n");
-        }
+    } while (choice != 0);
 
-    } while (choice != 0); // On boucle tant que le choix n'est pas 0
-
-    return 0;
+    return (0);
 }

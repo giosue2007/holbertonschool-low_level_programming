87597a1316d7629a29c2fb1a2b0b005bf1055962
giosue2007 <toxicgiosue@gmail>
2026-03-03 15:14:27 +0100

    add perm and c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - calcul la longueur d'une chaine de caractères
+ * @s: le pointeur vers la chaine (int)
+ * 
+ * Return: la longueur de la chaine (int)
+ */
+int _strlen(char *s)
+{
+	    int longue
+
+		longue = 0;
+		while (s)[longue] != '\0')
+		{
+			   longue++;
+		}
+
+		return (longue);
+}

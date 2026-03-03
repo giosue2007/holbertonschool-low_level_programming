5eee05709b6a91ecda332ae64a7161afa8caeb49
giosue2007 <toxicgiosue@gmail>
2026-03-03 14:59:01 +0100

    add perm and c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -7,9 +7,9 @@
  */
 void swap_int(int *a, int *b)
 {
-	int temp; // Le "verre vide" (variable temporaire)
+	int temp;
 
-	temp = *a; // On verse le contenu de A dans le verre vide
-	*a = *b;   // On verse le contenu de B dans A
-	*b = temp; // On verse le contenu du verre vide (l'ancien A) dans B
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }

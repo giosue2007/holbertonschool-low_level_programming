c3b767e651d123efba3687a7e4b4a6485cf3e462
giosue2007 <toxicgiosue@gmail>
2026-03-03 19:02:03 +0100

    add perm and c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,7 +4,7 @@
  * _atoi - convertit une chaine en entier
  * @s: la chaine a convertir
  *
- * Return: l'entier converti
+ * Return: l'entier converti, ou 0 si aucun chiffre n'est trouve
  */
 int _atoi(char *s)
 {
@@ -13,30 +13,31 @@ int _atoi(char *s)
 	unsigned int res = 0;
 	int found = 0;
 
-	/* 1. On parcourt la chaine */
 	while (s[i] != '\0')
 	{
-		/* 2. On gere les signes */
+		/* 1. On gere les signes : chaque '-' inverse le signe actuel */
 		if (s[i] == '-')
 		{
 			sign *= -1;
 		}
-		
-		/* 3. Si on trouve un chiffre */
-		if (s[i] >= '0' && s[i] <= '1' && s[i] <= '9')
+
+		/* 2. Si on trouve un chiffre (ASCII entre 48 et 57) */
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			found = 1;
-			/* On construit le nombre chiffre par chiffre */
+			/* On multiplie le resultat precedent par 10 pour decaler d'un rang */
+			/* Puis on ajoute la valeur numerique du caractere actuel */
 			res = (res * 10) + (s[i] - '0');
 		}
-		/* 4. Si on a deja trouve des chiffres et qu'on tombe sur autre chose */
-		else if (found)
+		/* 3. Si on avait commence a trouver des chiffres et qu'on voit autre chose */
+		else if (found == 1)
 		{
+			/* On s'arrete net ! C'est la regle d'atoi */
 			break;
 		}
 		i++;
 	}
 
-	/* 5. On applique le signe final */
+	/* 4. On multiplie le resultat positif par le signe final (1 ou -1) */
 	return (res * sign);
 }

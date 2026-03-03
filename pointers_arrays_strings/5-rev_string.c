5d62277a7068edb04a9893a33e728f583777bf7a
giosue2007 <toxicgiosue@gmail>
2026-03-03 16:01:01 +0100

    add perm and c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,32 @@
 #include "main.h"
 
 /**
- * rev_string - inverse une chaîne de caractères en mémoire
- * @s: la chaîne à modifier
+ * rev_string - inverse une chaine de caracteres
+ * @s: pointeur vers la chaine
  */
 void rev_string(char *s)
 {
 	int i, j, len;
 	char temp;
 
-	/* 1. On compte la longueur */
+	/* 1. Calcul de la longueur */
 	len = 0;
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
-	/* 2. On échange le premier avec le dernier, le 2ème avec l'avant-dernier... */
+	/* 2. On place i au debut et j a la fin */
 	i = 0;
 	j = len - 1;
 
+	/* 3. Echange des caracteres jusqu'au milieu */
 	while (i < j)
 	{
 		temp = s[i];
 		s[i] = s[j];
 		s[j] = temp;
+
 		i++;
 		j--;
 	}

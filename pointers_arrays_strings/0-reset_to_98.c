17a821611bb6ad689736e1032c693a47bb4609b1
giosue2007 <toxicgiosue@gmail>
2026-03-03 14:02:10 +0100

    add perm and c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - met à jour la valeur pointée par n à 98
+ * @n: pointeur vers l'entier à modifier
+ *
+ *	Return: void (rien
+ */
+void reset_to_98(int *n)
+{
+	    *n = 98;
+}

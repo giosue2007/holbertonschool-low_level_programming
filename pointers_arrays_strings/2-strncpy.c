3d8fe2e7fb68edd79c3651fbd77a8241ababf7a0
giosue2007 <toxicgiosue@gmail>
2026-03-04 15:55:57 +0100

    add perm and c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+/**
+ * _strncpy - Copie une chaine de caractères
+ * @dest: La destination
+ * @src: La source
+ * @n: Le nombre d'octets a copier
+ *
+ * Return: Un pointeur vers dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	/* ÉTAPE 1 : Copier src vers dest jusqu'à n ou la fin de src */
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	/* ÉTAPE 2 : Si n est plus grand que src, remplir le reste avec des \0 */
+	/* C'est la règle officielle de strncpy pour la sécurité */
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}

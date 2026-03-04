6f75165eb3d42a311311ed9766632195e1585198
giosue2007 <toxicgiosue@gmail>
2026-03-04 13:12:15 +0100

    add perm and c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,23 +8,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    /* On avance i jusqu'au bout de dest */
-    while (dest[i] != '\0')
-        i++;
+	/* On avance i jusqu'au bout de dest */
+	while (dest[i] != '\0')
+		i++;
 
-    /* On commence a copier src la ou on s'est arrete */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	/* On commence a copier src la ou on s'est arrete */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    /* On met le point final */
-    dest[i] = '\0';
+	/* On met le point final */
+	dest[i] = '\0';
 
-    return (dest);
+	return (dest);
 }

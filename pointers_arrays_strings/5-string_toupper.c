2a5ce1bcba16d197d7838d63310b121380ecb8f0
giosue2007 <toxicgiosue@gmail>
2026-03-05 10:28:26 +0100

    add perm and c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -4,24 +4,26 @@
  * string_toupper - Change toutes les minuscules d'une chaîne en majuscules.
  * @str: La chaîne de caractères à modifier.
  *
+ * Description: Parcourt la chaîne et soustrait 32 à la valeur ASCII
+ * de chaque lettre minuscule trouvée.
  * Return: Un pointeur vers la chaîne modifiée.
  */
 char *string_toupper(char *str)
 {
 	int i = 0;
 
-	/* On parcourt la chaîne jusqu'au caractère nul de fin '\0' */
+	/* Boucle jusqu'à la fin de la chaîne (caractère nul) */
 	while (str[i] != '\0')
 	{
-		/* Si le caractère est compris entre 'a' et 'z' */
+		/* Vérifie si le caractère actuel est une minuscule (a-z) */
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			/* On soustrait 32 pour obtenir la majuscule */
-			/* str[i] = str[i] - 32; est aussi possible */
-			str[i] -= 32;
+			/* Soustrait 32 pour passer de minuscule à majuscule */
+			str[i] = str[i] - 32;
 		}
 		i++;
 	}
 
 	return (str);
 }
+/* Assure-toi qu'il y a bien une ligne vide juste ici ! */

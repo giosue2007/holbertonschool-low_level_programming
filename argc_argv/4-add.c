2e886a40c460d6c47bd88cade0a449a4dcf5e1e0
giosue2007 <toxicgiosue@gmail>
2026-03-06 12:30:31 +0100

    add perm and c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -3,42 +3,35 @@
 
 /**
  * main - Additionne des nombres positifs passés en arguments.
- * @argc: Nombre d'arguments.
- * @argv: Tableau des arguments.
+ * @argc: Le nombre d'arguments passés au programme.
+ * @argv: Un tableau de chaînes de caractères (les arguments).
  *
- * Return: 0 si succès, 1 si un argument contient des symboles non-numériques.
+ * Return: 0 en cas de succès, 1 si un argument contient des
+ * symboles qui ne sont pas des chiffres.
  */
 int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	/* 1. Si aucun nombre n'est passé (seulement le nom du programme) */
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	/* 2. On parcourt chaque argument en commençant à 1 */
 	for (i = 1; i < argc; i++)
 	{
-		/* 3. On vérifie chaque caractère du mot actuel */
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			/* Si le caractère n'est pas un chiffre entre '0' et '9' */
 			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-
-		/* 4. Si le mot est valide, on le convertit et on l'ajoute */
 		sum += atoi(argv[i]);
 	}
 
-	/* 5. On affiche le résultat final */
 	printf("%d\n", sum);
-
 	return (0);
 }

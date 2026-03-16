2563ec42f334bf005bc8222ae765dc7e69edca52
giosue2007 <toxicgiosue@gmail>
2026-03-16 15:24:34 +0100

    add perm and c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,14 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Plan pour un chien
+ * @name: Nom
+ * @age: Âge
+ * @owner: Maître
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-/* CETTE LIGNE EST CRUCIALE */
+/* Le prototype (la signature de ta fonction) */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif

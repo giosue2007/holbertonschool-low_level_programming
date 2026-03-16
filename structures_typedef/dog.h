64c93bfb7510a17161213f3af668421b2c8929b0
giosue2007 <toxicgiosue@gmail>
2026-03-16 15:26:07 +0100

    add perm and c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,17 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - Plan pour créer un chien
- * @name: Nom
- * @age: Âge
- * @owner: Maître
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif

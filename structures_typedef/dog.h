92d470a59c2ffb5350e8b416d4d41337e94ce6f2
giosue2007 <toxicgiosue@gmail>
2026-03-16 13:48:36 +0100

    add perm and c

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * struct dog - Une structure pour décrire un chien
+ * @name: Le nom du chien (chaîne de caractères)
+ * @age: L'âge du chien (nombre à virgule)
+ * @owner: Le nom du propriétaire (chaîne de caractères)
+ */
+struc dog
+{
+	  char *name;
+	  float age;
+	  char *owner;
+}

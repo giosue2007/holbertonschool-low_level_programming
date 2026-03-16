d753730246ab1b641f19536d9459adf49e068af5
giosue2007 <toxicgiosue@gmail>
2026-03-16 15:22:44 +0100

    add perm and c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,19 +1,20 @@
-#include "dog.h"
-#include <stddef.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * init_dog - Initialise une variable de type struct dog
- * @d: Pointeur vers la structure dog à remplir
- * @name: Le nom à donner au chien
- * @age: L'âge à donner au chien
- * @owner: Le nom du propriétaire
+ * struct dog - Structure pour un chien
+ * @name: Nom
+ * @age: Âge
+ * @owner: Maître
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+/* AJOUTE CETTE LIGNE (le prototype) : */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif

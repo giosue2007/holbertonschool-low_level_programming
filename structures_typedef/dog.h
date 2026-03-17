20def5da9eb5cd418d4507ba6a84c88eac9ad5e9
giosue2007 <toxicgiosue@gmail>
2026-03-17 13:54:57 +0100

    add perm and c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure représentant un chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: nom du propriétaire
+ */
 typedef struct dog
 {
     char *name;
     float age;
     char *owner;
 } dog_t;
+
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */

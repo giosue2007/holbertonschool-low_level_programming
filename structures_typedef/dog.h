6c7b4a2f2780208c9017692a80bc4b34bd14b56c
giosue2007 <toxicgiosue@gmail>
2026-03-17 14:39:18 +0100

    fix dog.h add new_dog prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,11 +9,12 @@
  */
 typedef struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
-#endif
+#endif /* DOG_H */

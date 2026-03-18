4c97a84a5b1743539b7ec5d26a95aa68abceec02
giosue2007 <toxicgiosue@gmail>
2026-03-18 16:49:37 +0100

    add perm and c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,20 +1,16 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its parameters.
- * @n: The number of parameters passed to the function.
- * @...: A variable number of parameters to calculate the sum of.
- *
- * Return: If n == 0 - 0.
- * Otherwise - the sum of all parameters.
+ * sum_them_all - sums all its parameters
+ * @n: number of parameters
+ * Return: the sum of all parameters, or 0 if n == 0
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i, sum = 0;
-
-	if (n == 0)
-		return (0);
 
 	va_start(ap, n);
 

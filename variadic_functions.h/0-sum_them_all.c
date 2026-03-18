780b4a3eb34707f47a29c210d6da826a86c89752
giosue2007 <toxicgiosue@gmail>
2026-03-18 16:29:46 +0100

    add perm and c

--- variadic_functions.h/0-sum_them_all.c
+++ variadic_functions.h/0-sum_them_all.c
@@ -11,26 +11,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
+	va_list ap;
 	unsigned int i;
 	int sum = 0;
 
-	/* Condition CRUCIALE du sujet : si n est 0, on stop tout */
 	if (n == 0)
 		return (0);
 
-	/* On prépare la liste d'arguments après 'n' */
-	va_start(nums, n);
+	va_start(ap, n);
 
-	/* On boucle pour récupérer chaque nombre */
 	for (i = 0; i < n; i++)
-	{
-		/* On additionne en précisant que c'est un 'int' */
-		sum += va_arg(nums, int);
-	}
+		sum += va_arg(ap, int);
 
-	/* On nettoie proprement avant de partir */
-	va_end(nums);
+	va_end(ap);
 
 	return (sum);
 }

65d087d7cb12abc4fdc97b04b74271436881400c
giosue2007 <toxicgiosue@gmail>
2026-03-18 15:10:15 +0100

    add perm and c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,23 +1,34 @@
 #include "3-calc.h"
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int (*get_op_func(char *s))(int, int)
+int main(int argc, char *argv[])
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+	int num1, num2;
+	int (*func)(int, int);
 
-	while (ops[i].op != NULL)
+	if (argc != 4)
 	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
-		i++;
+		printf("Error\n");
+		exit(98);
 	}
-	return (NULL);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+
+	if (func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", func(num1, num2));
+	return (0);
 }

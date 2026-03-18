1ae7a09dc0f55f2732f5b8663da04b452bf39d31
giosue2007 <toxicgiosue@gmail>
2026-03-18 15:13:11 +0100

    add perm and c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,17 +1,22 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <string.h>
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add}, {"-", op_sub}, {"*", op_mul},
-		{"/", op_div}, {"%", op_mod}, {NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i = 0;
 
+	/* On parcourt le tableau jusqu'à trouver l'opérateur ou NULL */
 	while (ops[i].op != NULL)
 	{
+		/* LE SEUL IF AUTORISÉ : on compare le signe */
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,31 +4,34 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int a, b;
 	int (*func)(int, int);
 
+	/* IF 1 : Vérifie le nombre d'arguments */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	func = get_op_func(argv[2]);
 
+	/* IF 2 : Vérifie si l'opérateur est valide (ex: pas de "++" ou "z") */
 	if (func == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	/* IF 3 : Vérifie la division ou le modulo par zéro */
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", func(num1, num2));
+	printf("%d\n", func(a, b));
 	return (0);
 }

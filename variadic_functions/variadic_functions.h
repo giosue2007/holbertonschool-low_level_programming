1855487a00b2a3ca9c2c3801a6a134b92e1a1752
giosue2007 <toxicgiosue@gmail>
2026-03-18 16:10:09 +0100

    add perm and c

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+/* Ajoute les autres prototypes des exercices suivants ici plus tard */
+
+#endif

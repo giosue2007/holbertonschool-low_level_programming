866f9b2b1d09c720728f123aeb3f6f10495c626c
giosue2007 <toxicgiosue@gmail>
2026-03-18 15:36:22 +0100

    add perm and c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,32 +1,42 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - Effectue des opérations simples.
+ * @argc: Nombre d'arguments.
+ * @argv: Tableau d'arguments.
+ *
+ * Return: 0 en cas de succès.
+ */
 int main(int argc, char *argv[])
 {
 	int a, b;
 	int (*f)(int, int);
 
-	/* 1. D'abord on vérifie le nombre d'arguments */
+	/* 1. Vérification du nombre d'arguments (98) */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	/* 2. On cherche la fonction correspondant à l'opérateur */
+	/* On récupère la fonction avant de tester quoi que ce soit d'autre */
 	f = get_op_func(argv[2]);
 
-	/* --- C'EST ICI QUE TU METS LE CORRECTIF POUR LE CHECK 12 --- */
+	/* 2. Vérification de l'opérateur (99) */
+	/* f == NULL : l'opérateur n'est pas dans la liste */
+	/* argv[2][1] != '\0' : l'opérateur fait plus d'un caractère (ex: "++") */
 	if (f == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	/* ----------------------------------------------------------- */
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	/* 3. Enfin, on vérifie la division par zéro */
+	/* 3. Vérification division/modulo par zéro (100) */
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");
@@ -34,5 +44,6 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", f(a, b));
+
 	return (0);
 }

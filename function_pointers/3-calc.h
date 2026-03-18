b39c32cc0e4a73edce76bba9e3117e5e6f27d1a4
giosue2007 <toxicgiosue@gmail>
2026-03-18 15:14:19 +0100

    add perm and c

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,29 +1,17 @@
 #ifndef CALC_H
 #define CALC_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-/**
- * struct op - Structure pour une opération
- * @op: L'opérateur (ex: "+")
- * @f: La fonction associée
- */
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+    char *op;
+    int (*f)(int a, int b);
 } op_t;
 
-/* Prototypes des fonctions de calcul (3-op_functions.c) */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
-/* Prototype de la fonction de sélection (3-get_op_func.c) */
 int (*get_op_func(char *s))(int, int);
 
 #endif

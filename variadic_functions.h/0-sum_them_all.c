eae31f0bffc725d0527faa0c9c40b3e50f69fe44
giosue2007 <toxicgiosue@gmail>
2026-03-18 16:35:44 +0100

    Fix requirements for task 0

--- variadic_functions.h/0-sum_them_all.c
+++ variadic_functions.h/0-sum_them_all.c
@@ -2,26 +2,27 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - returns the sum of all its parameters
- * @n: number of parameters passed to the function
- * @...: a variable number of parameters to calculate the sum of
+ * sum_them_all - Returns the sum of all its parameters.
+ * @n: The number of parameters passed to the function.
+ * @...: A variable number of parameters to calculate the sum of.
  *
- * Return: if n == 0 - 0, otherwise the sum of all parameters
+ * Return: If n == 0 - 0.
+ * Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
+	va_list nums;
 	unsigned int i, sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(ap, n);
+	va_start(nums, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+		sum += va_arg(nums, int);
 
-	va_end(ap);
+	va_end(nums);
 
 	return (sum);
 }

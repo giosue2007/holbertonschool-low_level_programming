417be8e0ce6d925a1a42fbf6d1b411130eab05ff
giosue2007 <toxicgiosue@gmail>
2026-03-18 15:23:34 +0100

    add perm and c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,26 +1,49 @@
-#ifndef CALC_H
-#define CALC_H
-
-#include <stdlib.h>
+#include "3-calc.h"
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 /**
- * struct op - Struct op
- * @op: The operator
- * @f: The function associated
+ * main - Effectue des opérations simples.
+ * @argc: Nombre d'arguments.
+ * @argv: Tableau d'arguments.
+ *
+ * Return: 0 en cas de succès.
  */
-typedef struct op
+int main(int argc, char *argv[])
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	int a, b;
+	int (*f)(int, int);
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+	/* 1. Vérification du nombre d'arguments (98) */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-#endif
+	/* On récupère la fonction avant de tester quoi que ce soit d'autre */
+	f = get_op_func(argv[2]);
+
+	/* 2. Vérification de l'opérateur (99) */
+	/* f == NULL : l'opérateur n'est pas dans la liste */
+	/* argv[2][1] != '\0' : l'opérateur fait plus d'un caractère (ex: "++") */
+	if (f == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	/* 3. Vérification division/modulo par zéro (100) */
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", f(a, b));
+
+	return (0);
+}

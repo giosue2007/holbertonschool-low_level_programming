82bfd26e62d466b06fd4f3d4e087f4cd2727337d
giosue2007 <toxicgiosue@gmail>
2026-03-18 14:41:19 +0100

    add perm and c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: function pointer
+ * print_name - Affiche un nom.
+ * @name: Le nom à afficher (chaîne de caractères).
+ * @f: Pointeur vers la fonction de rappel (callback) pour l'affichage.
+ *
+ * Return: Rien (void).
  */
 void print_name(char *name, void (*f)(char *))
 {

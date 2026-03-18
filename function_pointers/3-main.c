94dbb918483ad63ecd8e06e9252c4528f30e6cc0
giosue2007 <toxicgiosue@gmail>
2026-03-18 15:19:45 +0100

    add perm and c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,13 +1,10 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 	int a, b;
-	int (*func)(int, int);
+	int (*f)(int, int);
 
-	/* IF 1 : Vérifie le nombre d'arguments */
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -16,22 +13,20 @@ int main(int argc, char *argv[])
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	func = get_op_func(argv[2]);
+	f = get_op_func(argv[2]);
 
-	/* IF 2 : Vérifie si l'opérateur est valide (ex: pas de "++" ou "z") */
-	if (func == NULL || argv[2][1] != '\0')
+	if (f == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	/* IF 3 : Vérifie la division ou le modulo par zéro */
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", func(a, b));
+	printf("%d\n", f(a, b));
 	return (0);
 }

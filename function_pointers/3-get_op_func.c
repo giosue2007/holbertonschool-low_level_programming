ee8c2fe7c1c6333f9a2ad825a1797e9dd20506f2
giosue2007 <toxicgiosue@gmail>
2026-03-18 15:40:28 +0100

    add perm and c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,8 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		/* On compare le premier caractère ET on vérifie que s n'a qu'un seul carac */
-		if (s[0] == ops[i].op[0] && s[1] == '\0')
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}

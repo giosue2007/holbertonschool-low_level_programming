d1cf0b0adf2b0520d377d40bf22d133255d22303
giosue2007 <toxicgiosue@gmail>
2026-03-18 16:12:18 +0100

    add perm and c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,34 +2,22 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - fait la somme de tous ses paramètres
- * @n: le nombre d'arguments envoyés
- * @...: les nombres à additionner
- *
- * Return: la somme totale, ou 0 si n est nul
+ * sum_them_all - sums all its parameters
+ * @n: number of parameters
+ * Return: the sum of all parameters, or 0 if n == 0
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list panier;
-	unsigned int i;
-	int somme = 0;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	/* Si n est 0, le sujet dit de retourner 0 */
-	if (n == 0)
-		return (0);
+	va_start(ap, n);
 
-	/* 1. Initialisation : on commence après 'n' */
-	va_start(panier, n);
-
-	/* 2. On boucle n fois pour tout récupérer */
 	for (i = 0; i < n; i++)
-	{
-		/* On pioche un 'int' et on l'ajoute */
-		somme += va_arg(panier, int);
-	}
+		sum += va_arg(ap, int);
 
-	/* 3. On nettoie tout avant de finir */
-	va_end(panier);
+	va_end(ap);
 
-	return (somme);
+	return (sum);
 }

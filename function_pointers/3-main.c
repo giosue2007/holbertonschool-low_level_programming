86db3aae4baaf3e56f5c031f60d7e46121a56bec
giosue2007 <toxicgiosue@gmail>
2026-03-18 15:37:17 +0100

    Final fix for check 12

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,32 +1,28 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Effectue des opérations simples.
- * @argc: Nombre d'arguments.
- * @argv: Tableau d'arguments.
+ * main - Performe des opérations simples
+ * @argc: Nombre d'arguments
+ * @argv: Tableau d'arguments
  *
- * Return: 0 en cas de succès.
+ * Return: 0 (Succès)
  */
 int main(int argc, char *argv[])
 {
 	int a, b;
 	int (*f)(int, int);
 
-	/* 1. Vérification du nombre d'arguments (98) */
+	/* Check 98: Nombre d'arguments */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	/* On récupère la fonction avant de tester quoi que ce soit d'autre */
+	/* Récupération de la fonction */
 	f = get_op_func(argv[2]);
 
-	/* 2. Vérification de l'opérateur (99) */
-	/* f == NULL : l'opérateur n'est pas dans la liste */
-	/* argv[2][1] != '\0' : l'opérateur fait plus d'un caractère (ex: "++") */
+	/* Check 99: Opérateur invalide ou trop long (LE fameux Check 12) */
 	if (f == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
@@ -36,7 +32,7 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	/* 3. Vérification division/modulo par zéro (100) */
+	/* Check 100: Division ou Modulo par zéro */
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");

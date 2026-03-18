5ac5f7938562ff0556f11fc37c5439d9188de9a4
giosue2007 <toxicgiosue@gmail>
2026-03-18 15:25:49 +0100

    add perm and c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,49 +1,43 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Effectue des opérations simples.
- * @argc: Nombre d'arguments.
- * @argv: Tableau d'arguments.
+ * main - Effectue l'operation demandee
+ * @argc: Nombre d'arguments
+ * @argv: Tableau d'arguments
  *
- * Return: 0 en cas de succès.
+ * Return: 0 (Succes)
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int a, b, result;
 	int (*f)(int, int);
 
-	/* 1. Vérification du nombre d'arguments (98) */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	/* On récupère la fonction avant de tester quoi que ce soit d'autre */
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
 	f = get_op_func(argv[2]);
 
-	/* 2. Vérification de l'opérateur (99) */
-	/* f == NULL : l'opérateur n'est pas dans la liste */
-	/* argv[2][1] != '\0' : l'opérateur fait plus d'un caractère (ex: "++") */
 	if (f == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	/* 3. Vérification division/modulo par zéro (100) */
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", f(a, b));
+	result = f(a, b);
+
+	printf("%d\n", result);
 
 	return (0);
 }

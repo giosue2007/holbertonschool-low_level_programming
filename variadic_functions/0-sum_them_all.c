781658f437216658316b0b1eef77eb548cacdc58
giosue2007 <toxicgiosue@gmail>
2026-03-20 11:05:21 +0100

    add perm and c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,22 +1,33 @@
-#include "variadic_functions.h"
+#include "variadic_function.h"
 #include <stdarg.h>
 
 /**
- * sum_them_all - sums all its parameters
- * @n: number of parameters
- * Return: the sum of all parameters, or 0 if n == 0
+ * sum_them_all - Calcule la somme de tous ses paramètres.
+ * @n: Le nombre de paramètres passés à la fonction.
+ * @...: Les paramètres à additionner.
+ *
+ * Return: La somme de tous les paramètres, ou 0 si n == 0.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i, sum = 0;
+	unsigned int i;
+	int sum = 0;
 
+	if (n == 0)
+		return (0);
+
+	/* Initialise la liste d'arguments avec le dernier paramètre fixe n */
 	va_start(ap, n);
 
+	/* Parcourt tous les arguments fournis */
 	for (i = 0; i < n; i++)
+	{
+		/* Récupère l'argument suivant de type int et l'ajoute à la somme */
 		sum += va_arg(ap, int);
+	}
 
+	/* Nettoie la liste d'arguments */
 	va_end(ap);
 
 	return (sum);
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Prototype de la fonction de somme
+ * @n: Nombre d'arguments
+ * @...: Arguments variables
+ * * Return: La somme des arguments
+ */
+int sum_them_all(const unsigned int n, ...);
+
+/* Tu ajouteras les prochains prototypes ici (ex: print_numbers, etc.) */
+
+#endif /* VARIADIC_FUNCTIONS_H */

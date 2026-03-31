80f1b51af0840215965d03b81af37761d96e7659
giosue2007 <toxicgiosue@gmail>
2026-03-31 16:34:26 +0200

    add perm and c

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implémentation de l'algorithme djb2
- * @str: chaîne de caractères à hacher
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
  *
- * Return: la valeur du hash
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {

1768b87ad1d40eae762fba9299434fa69ab9e03b
giosue2007 <toxicgiosue@gmail>
2026-03-31 16:09:12 +0200

    add perm and c

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -20,5 +20,6 @@ typedef struct dlistint_s
 void free_dlistint(dlistint_t *head);
 size_t print_dlistint(const dlistint_t *h);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head)
 
 #endif

cecba4de53ad6c2e1e767c92b854c5dcc3abbf5a
giosue2007 <toxicgiosue@gmail>
2026-03-31 15:37:00 +0200

    add perm and c

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -3,4 +3,7 @@ typedef struct dlistint_s
     int n;
     struct dlistint_s *prev;
     struct dlistint_s *next;
-} dlistint_t;
+} dlistint_t; /* <--- Vérifie bien que le ';' est ici */
+
+/* Prototipo della tua funzione */
+size_t print_dlistint(const dlistint_t *h);

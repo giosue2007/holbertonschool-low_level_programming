1d8d7aee039f8db1078c1861939f22ff71ede4db
giosue2007 <toxicgiosue@gmail>
2026-03-31 16:00:08 +0200

    add perm and c

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -5,14 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- */
 typedef struct dlistint_s
 {
 	int n;
@@ -23,6 +15,6 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif

40b91bd328392df13338ecf3be4c45ffffd10256
giosue2007 <toxicgiosue@gmail>
2026-03-31 16:03:05 +0200

    add perm and c

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,10 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ */
 typedef struct dlistint_s
 {
 	int n;
@@ -12,10 +16,9 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+/* Ajoute bien l'étoile * et le point-virgule ; */
+void free_dlistint(dlistint_t *head);
 size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head)
+/* ... autres prototypes ... */
 
 #endif

0ea4f7ebb450bd9ba9f34cf3e64388262ea35df5
giosue2007 <toxicgiosue@gmail>
2026-03-31 16:10:22 +0200

    add perm and c

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -16,10 +18,13 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-/* Ajoute bien l'étoile * et le point-virgule ; */
-void free_dlistint(dlistint_t *head);
+/* Vérifie bien les étoiles (*) et les points-virgules (;) */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-int sum_dlistint(dlistint_t *head)
+int sum_dlistint(dlistint_t *head);
 
-#endif
+#endif /* LISTS_H */

18e92749a6a0936175937801750bb0918cd87313
giosue2007 <toxicgiosue@gmail>
2026-04-01 15:49:18 +0200

    add perm and c

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,8 +2,8 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -24,10 +24,9 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-/* Prototypes des tâches 0, 1 et 2 */
+/* Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 #endif /* HASH_TABLES_H */

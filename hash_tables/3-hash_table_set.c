24b01bae6f51901ce213f47ef8099256cd57a545
giosue2007 <toxicgiosue@gmail>
2026-04-01 15:50:21 +0200

    add perm and c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,62 +1,71 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - ajoute ou met à jour un élément dans la table
- * @ht: la table de hachage
- * @key: la clé (ne peut pas être vide)
- * @value: la valeur associée (doit être dupliquée)
+ * create_node - Crée un nouveau nœud pour la table de hachage
+ * @key: La clé
+ * @value: La valeur
+ *
+ * Return: Le nouveau nœud ou NULL si échec
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (NULL);
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (NULL);
+	}
+	new->next = NULL;
+	return (new);
+}
+
+/**
+ * hash_table_set - Ajoute ou met à jour un élément
+ * @ht: La table de hachage
+ * @key: La clé
+ * @value: La valeur
  *
  * Return: 1 si succès, 0 sinon
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *temp;
 	unsigned long int index;
-	char *new_value;
+	hash_node_t *new, *temp;
+	char *val_copy;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (!ht || !key || !*key || !value)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
-
-	/* 1. Vérifier si la clé existe déjà pour la mettre à jour */
 	temp = ht->array[index];
 	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
-			new_value = strdup(value);
-			if (new_value == NULL)
+			val_copy = strdup(value);
+			if (!val_copy)
 				return (0);
 			free(temp->value);
-			temp->value = new_value;
+			temp->value = val_copy;
 			return (1);
 		}
 		temp = temp->next;
 	}
-
-	/* 2. Si la clé n'existe pas, créer un nouveau nœud */
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	new = create_node(key, value);
+	if (!new)
 		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
-	/* 3. Insérer au début de la liste (gestion de collision) */
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
+	new->next = ht->array[index];
+	ht->array[index] = new;
 	return (1);
 }
